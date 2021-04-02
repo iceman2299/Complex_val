71d3e93d85a190385931d0b82c3dcd0f0a07ec36
iceman2299 <58778213+iceman2299@users.noreply.github.com>
2021-04-02 15:33:46 +0300

    Add files via upload

--- Complex_val/Complex.h
+++ Complex_val/Complex.h
@@ -10,7 +10,12 @@ public:
 	Complex();
 	Complex(int _re, int _im);
 	Complex(const Complex& complex);
-	int getre();
-	int getim();
+	int getre() const;
+	int getim() const;
+	void setre(int);
+	void setim(int);
+	Complex operator+(const Complex& b);
+	Complex(int _re);
+
 };
 
--- Complex_val/Complex_val.cpp
+++ Complex_val/Complex_val.cpp
@@ -3,21 +3,39 @@
 #include <clocale>
 #include "Complex.h"
 
-int Complex::getre() { return re; };
-int Complex::getim() { return im; };
+int Complex::getre() const { return re; };
+int Complex::getim() const { return im; };
+void Complex::setre(int _re)  {re = _re;};
+void Complex::setim(int _im) {im = _im;};
+
+Complex Complex::operator+(const Complex& b)
+{
+	Complex res;
+	res.re = re + b.re;
+	res.im = im + b.im;
+	std::cout << "\x1b[1;35mПройдена перегрузка оператора+\x1b[1;0m" << std::endl;
+	return res;
+}
+
+Complex::Complex(int _re)
+{
+	re = _re;
+	im = 0;
+	std::cout << "\x1b[1;30mПройден конструктор преобразования типа\x1b[1;0m" << std::endl;
+}
 
 Complex::Complex()
 {
 	re = 0;
 	im = 0;
-	std::cout << "Пройден конструктор по умолчанию" << std::endl;
+	std::cout << "\x1b[1;34mПройден конструктор по умолчанию\x1b[1;0m" << std::endl;
 }
 
 Complex::Complex(int _re, int _im)
 {
 	re = _re;
 	im = _im;
-	std::cout << "Пройден конструктор инициализации" << std::endl;
+	std::cout << "\x1b[1;33mПройден конструктор инициализации\x1b[1;0m" << std::endl;
 }
 
 
@@ -25,15 +43,15 @@ Complex::Complex(const Complex& complex)
 {
 	re = complex.re;
 	im = complex.im;
-	std::cout << "Пройден конструктор копирования" << std::endl;
+	std::cout << "\x1b[1;36mПройден конструктор копирования\x1b[1;0m" << std::endl;
 };
 
-Complex summa(Complex complex1, Complex complex2)
+Complex summa(const Complex& complex1, const Complex& complex2)
 {
 	Complex c;
-	c. = complex1.getre() + complex2.getre();
-	c. = complex1.getim() + complex2.getim();
-	std::cout << "Функция сложения пройдена" << std::endl;
+	c.setre(complex1.getre() + complex2.getre());
+	c.setim(complex1.getim() + complex2.getim());
+	std::cout << "\x1b[1;32mФункция сложения пройдена\x1b[0m" << std::endl;
 	return c;
 }
 
@@ -50,7 +68,11 @@ int main()
 	Complex b(a);
 	std::cout << "Второе число: " << b.getre() << "+" << b.getim() << "i" << std::endl;
 	Complex c = summa(a, b);
-	std::cout << "Результат сложения этих двух чисел: " << c.getre() << "+" << c.getim() << "i" << std::endl;
+	std::cout << "Результат сложения этих двух чисел через функцию: " << c.getre() << "+" << c.getim() << "i" << std::endl;
+	Complex d = a + c;
+	std::cout << "Результат сложения первого числа и предыдущего результата: " << d.getre() << "+" << d.getim() << "i" << std::endl;
+	Complex k = d + 3;
+	std::cout << "Результат сложения предыдущего результата и числа 3: " << k.getre() << "+" << k.getim() << "i" << std::endl;
 	system("pause");
 }
 
